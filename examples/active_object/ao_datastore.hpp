d81ab7e43e953574ca9a1b5140143c35931c17a0
brian baldino <bbaldino@gmail.com>
2016-02-14 20:49:36 -0800

    add active object example (shows Post)

---
+++ examples/active_object/ao_datastore.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <boost/optional.hpp>
+#include <list>
+
+#include "scheduler.hpp"
+
+// Rather than synchronizing access to data_ via locks, it can instead be done
+//  by posting all operations on it to a single thread.
+template<typename Key, typename Value>
+class AODataStore {
+public:
+  void AddValue(Key key, Value value) {
+    return scheduler_.Post(boost::bind(&AODataStore::_DoAddValue, this, key, value));
+  }
+
+  boost::optional<Value> GetValue(int key) {
+    auto res = scheduler_.Post(boost::bind(&AODataStore::_DoGetValue, this, key));
+    // We could just return the future here, but doing it this way totally hides
+    //  the asynchronous behavior of the AODataStore implementation from the user
+    return res.get();
+  }
+
+  void RemoveValue(int key) {
+    return scheduler_.Post(boost::bind(&AODataStore::_DoRemoveValue, this, key));
+  }
+
+//protected:
+  void _DoAddValue(Key key, Value value) {
+    data_[key] = value;
+  }
+
+  boost::optional<Value> _DoGetValue(int key) {
+    if (data_.find(key) != data_.end()) {
+      return data_[key];
+    }
+    return boost::none;
+  }
+
+  void _DoRemoveValue(int key) {
+    data_.erase(key);
+  }
+
+  Scheduler scheduler_;
+  std::map<Key, Value> data_;
+};
---
+++ examples/active_object/buffer.cc
@@ -0,0 +1,55 @@
+#include <cstdlib>
+
+#include "ao_datastore.hpp"
+
+#include <boost/thread.hpp>
+
+
+int main(int argc, char* argv[]) {
+  AODataStore<int, int> data;
+  bool running_ = true;
+
+  boost::thread adder([&]() {
+    int i = 0;
+    while (running_) {
+      boost::this_thread::sleep(boost::posix_time::milliseconds(200));
+      data.AddValue(i, i);
+      printf("added value %d for key %d\n", i, i);
+      i = i >= 10 ? 0 : i + 1; 
+    }
+  });
+
+  boost::thread reader([&]() {
+    int i = 0;
+    while (running_) {
+      boost::this_thread::sleep(boost::posix_time::milliseconds(300));
+      auto val = data.GetValue(i);
+      if (val) {
+        printf("got value %d for key %d\n", val.get(), i);
+      } else {
+        printf("no value for key %d\n", i);
+      }
+      i = i >= 10 ? 0 : i + 1; 
+    }
+  });
+
+  boost::thread remover([&]() {
+    std::srand(std::time(0));
+    while (running_) {
+      boost::this_thread::sleep(boost::posix_time::milliseconds(600));
+      int rand_key = std::rand() % 10;
+      printf("removing value for key %d\n", rand_key);
+      data.RemoveValue(rand_key);
+    }
+  });
+
+
+  boost::this_thread::sleep(boost::posix_time::seconds(10));
+  running_ = false;
+  adder.join();
+  reader.join();
+  remover.join();
+
+
+  return 0;
+}
